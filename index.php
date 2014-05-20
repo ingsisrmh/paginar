af1b118f3ea7bb8ee235da84fdf40d67613197be
Ricardo Mendoza Huesca <ingsisrmh@gmail.com>
2014-05-20 13:42:51 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCUMENT html>
+
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>paginacion</title>
+	<script src="paginar.js"></script>
+	<link rel="stylesheet" href="paginar.css">
+</head>
+
+<body>
+	<div id="paginacion"></div>
+</body>
+</html>
